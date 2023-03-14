09b93788e7c275f99888a2c4385881e4f686a0a7
youssefjn <youssefjneyah@gmail.com>
2023-03-14 12:47:22 +0100

    first commit

---
+++ src/main/java/com/example/okta2/Okta2Application.java
@@ -0,0 +1,13 @@
+package com.example.okta2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Okta2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Okta2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/okta2/Okta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Okta2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
